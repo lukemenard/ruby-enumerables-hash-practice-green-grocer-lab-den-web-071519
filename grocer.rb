63251759afd24484a37add5f749316c629c16352
Luke Menard <lukemenard@gmail.com>
2019-07-12 21:30:42 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,57 @@
 def consolidate_cart(cart)
-  # code here
+  clean_cart = {}
+  cart.each do |item|
+    item.each do |x, info|
+      if clean_cart[x]
+        clean_cart[x][:count] += 1
+      else
+        clean_cart[x] = info
+        clean_cart[x][:count] = 1
+      end
+    end
+  end
+  clean_cart
 end
 
-def apply_coupons(cart, coupons)
-  # code here
+def apply_coupons(cart, coupons) 
+  coupons.each do |coupon| 
+    coupon.each do |attribute, value| 
+      item = coupon[:item] 
+      if cart[item] && cart[item][:count] >= coupon[:num] 
+        if cart["#{item} W/COUPON"] 
+          cart["#{item} W/COUPON"][:count] += coupon[:num] 
+        else
+          cart["#{item} W/COUPON"] = {:price => coupon[:cost], 
+          :clearance => cart[item][:clearance], :count => coupon[:num]} 
+          cart["#{item} W/COUPON"][:price] = (cart["#{item} W/COUPON"][:price] / coupon[:num])
+        end 
+      cart[item][:count] -= coupon[:num]
+    end 
+  end 
+end 
+  cart 
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |item, hash|
+    if hash[:clearance] == true
+      hash[:price] = (hash[:price]*0.8).round(2)
+    end
+  end
+  cart
 end
 
 def checkout(cart, coupons)
-  # code here
+  total_price = 0
+  clean_cart = consolidate_cart(cart)
+  coupon_cart = apply_coupons(clean_cart, coupons)
+  clearance_cart = apply_clearance(coupon_cart)
+  
+  clearance_cart.each do |item, hash|
+    total_price =+ (hash[:count] * hash[:price])
+  end
+  if total_price > 100
+    total_price = total_price*0.9
+  end
+  total_price
 end
