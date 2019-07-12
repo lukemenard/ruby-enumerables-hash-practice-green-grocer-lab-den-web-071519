aadd346beeeaa68290b785c6ddacc9aa718b9d96
Luke Menard <lukemenard@gmail.com>
2019-07-12 22:16:45 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -14,12 +14,13 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons) 
-  hash = cart
-  
   coupons.each do |coupon| 
+    item = coupon[:item] 
+    if cart[item] && cart[item][:count] >= coupon[:num]
+    
+    
     coupon.each do |key, value| 
-      item = coupon[:item] 
-      if cart[item] && cart[item][:count] >= coupon[:num] 
+       
         if cart["#{item} W/COUPON"] 
           cart["#{item} W/COUPON"][:count] += coupon[:num] 
         else
